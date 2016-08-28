017fd97e412b09aebe06f23d2c44102eae8a6c09
ericxiao251 <xiaoe@mcmaster.ca>
2016-08-28 18:28:28 -0400

    Changed implementation of RegisterView.

--- CPUSimulator/CPUViewController.swift
+++ CPUSimulator/CPUViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class CPUViewController: UIViewController {
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated
     }
     
+    // TODO with portions
+    var registerView = RegisterView(frame: CGRect(x: 450, y: 700, width: 0, height: 0))
+    
     override func viewDidLoad() {
+        // Do any additional setup after loading the view, typically from a nib.
         super.viewDidLoad()
-        //        // Do any additional setup after loading the view, typically from a nib.
+        super.view.addSubview(registerView)
     }
 
 }
-
--- CPUSimulator/RegisterView.swift
+++ CPUSimulator/RegisterView.swift
@@ -19,9 +19,15 @@ class RegisterView: UIView {
     var registerLabels = [UILabel]()
     var registerValues = [UILabel]()
     
-    // TODO: wtf is a NSCoder
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    // TODO initializing with a view in the story board
+    // required init?(coder aDecoder: NSCoder) {
+    
+    // TODO initializing with making an instance of the UIView Class in Controller
+    override init(frame: CGRect) {
+        let frameWidth = (4 * labelWidth) + (4 * margin)
+        let frameHeight = (2 * labelHeight) + (2 * valueHeight) + (3 * margin)
+        
+        super.init(frame: CGRect(x: Int(frame.origin.x), y: Int(frame.origin.y), width: frameWidth, height: frameHeight))
         
         for _ in 0..<8 {
             let label = UILabel()
@@ -42,24 +48,29 @@ class RegisterView: UIView {
         
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
     // update the register label frames and positon
     override func layoutSubviews() {
         var labelFrame = CGRect(x: 0, y:0, width: labelWidth, height: labelHeight)
         var valueFrame = CGRect(x: 0, y:0, width: labelWidth, height: labelHeight)
-        for (index) in 0..<8 {
+        for index in 0..<registerValues.count {
             if(index < 4) {
-                labelFrame.origin.x = CGFloat(index * (labelWidth) )
+                labelFrame.origin.x = CGFloat(index * (labelWidth + margin))
                 
-                valueFrame.origin.y = CGFloat(labelHeight + margin)
-                valueFrame.origin.x = CGFloat(index * (valueWidth))
+                valueFrame.origin.x = CGFloat(index * (valueWidth + margin))
+                valueFrame.origin.y = CGFloat(margin + labelHeight)
             }
             // switch row after 4 registers
             else {
-                labelFrame.origin.y = CGFloat(labelHeight + valueHeight + margin)
-                labelFrame.origin.x = CGFloat((index - 4) * (labelWidth))
-                 
-                valueFrame.origin.y = CGFloat(2 * labelHeight + valueHeight + margin)
-                valueFrame.origin.x = CGFloat((index - 4) * (valueWidth))
+                labelFrame.origin.x = CGFloat((index - 4) * (labelWidth + margin))
+                labelFrame.origin.y = CGFloat(labelHeight + valueHeight + 2 * margin)
+                
+                valueFrame.origin.x = CGFloat((index - 4) * (valueWidth + margin))
+                valueFrame.origin.y = CGFloat(2 * labelHeight + valueHeight + 3 * margin)
             }
             registerLabels[index].frame = labelFrame
             registerValues[index].frame = valueFrame
